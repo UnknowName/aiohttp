423a40806300890a90b5b807746bb0a03959f5b3
UnknowName <burtte@sina.com>
2019-06-04 14:20:17 +0800

    loop.close() delete

--- tests/test_wechat_utils.py
+++ tests/test_wechat_utils.py
@@ -8,7 +8,6 @@ def async_wrapper(coro):
     def wrapper(*args, **kwargs):
         loop = asyncio.get_event_loop()
         return loop.run_until_complete(coro(*args, **kwargs))
-        loop.close()
     return wrapper
 
 
