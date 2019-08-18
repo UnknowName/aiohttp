d4eeae5290260382e4d2a269fafb0787d346572e
UnknowName <burtte@sina.com>
2019-08-18 14:19:11 +0800

    Rewrite Command Thread method

--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -3,13 +3,31 @@ from subprocess import run, STDOUT, PIPE
 from asyncio.coroutines import iscoroutinefunction
 
 
-class BaseCommand(Thread):
-    def __init__(self, cmd: str, notify_func, users: list):
+class _BaseCommand(Thread):
+    # 这个类属性后面需要被继承的各类重写
+    _command_template = "{}{}"
+
+    def __init__(self, cmd: str, host: str, service: str, notify_func, users: list):
         Thread.__init__(self)
 
         self._cmd = cmd
+        self.host = host
+        self.service = service
         self._func = notify_func
         self._users = users
+
+    def _create_task_yml(self) -> str:
+        task_str = self._command_template.format(host=self.host, service=self.service)
+        yaml_name = "{}_{}.yml".format(self.host, self.service)
+        with open(yaml_name, "w") as f:
+            f.write(task_str)
+            return yaml_name
+
+    def _get_cmd(self) -> str:
+        if self.host and self.service:
+            play_book = self._create_task_yml()
+            return "ansible-playbook {}".format(play_book)
+        return self._cmd
 
     @staticmethod
     def execute_cmd(cmd: str) -> str:
@@ -20,7 +38,8 @@ class BaseCommand(Thread):
             return stdout.decode("gbk")
 
     def run(self) -> None:
-        cmd_output = self.execute_cmd(self._cmd)
+        cmd = self._get_cmd()
+        cmd_output = self.execute_cmd(cmd)
         if iscoroutinefunction(self._func):
             import asyncio
             loop = asyncio.new_event_loop()
@@ -31,71 +50,50 @@ class BaseCommand(Thread):
             self._func(self._users, cmd_output)
 
 
-class RecycleCommand(BaseCommand):
-    _RECYCLE_TEMPLATE = """---
+class _RecycleCommand(_BaseCommand):
+    _command_template = r"""
+    ---
     - hosts:
       - {host}
       gather_facts: False
       tasks:
-      - name: Restart {domain} IIS WebApplicationPool
+      - name: Restart {service} IIS WebApplicationPool
         win_iis_webapppool:
-          name: {domain}
+          name: {service}.sissyun.com.cn
           state: restarted
     """
 
-    def _create_task_yaml(self, host: str, domain: str) -> str:
-        task_str = self._RECYCLE_TEMPLATE.format(host=host, domain=domain)
-        yaml_name = "{}_{}.yml".format(host, domain)
-        with open(yaml_name, "w") as f:
-            f.write(task_str)
-            return yaml_name
 
-    def convert_cmd(self, origin_cmd: str) -> str:
-        if origin_cmd.startswith("recycle"):
-            cmd, host, app = origin_cmd.split(" ")
-            domain = '{}.sissyun.com.cn'.format(app)
-            filename = self._create_task_yaml(host, domain)
-            return "ansible-playbook {}".format(filename)
-        return origin_cmd
-
-    def run(self):
-        ansible_cmd = self.convert_cmd(self._cmd)
-        cmd_output = self.execute_cmd(ansible_cmd)
-        if iscoroutinefunction(self._func):
-            import asyncio
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(self._func(self._users, cmd_output))
-            loop.close()
-        else:
-            self._func(self._users, cmd_output)
-
-
-class ServicesCommand(BaseCommand):
-    _command_template = """---
-    - host: {host}
+class ServicesCommand(_BaseCommand):
+    _command_template = r"""
+    ---
+    - host:
+      - {host}
+      gather_facts: False
       tasks:
       - name: Restart docker-compose service {service_name}
         shell: cd /data/${service_name}&&docker-compose restart
     """
 
 
-class _SystemCommandThread(BaseCommand):
+class _SystemCommandThread(_BaseCommand):
     """Execute System Command"""
     pass
 
 
-class _RunWindowsProcess(BaseCommand):
+class _RunWindowsProcess(_BaseCommand):
     """Run a windows .exe file"""
-    _template_fmt = r"""---
+    _command_template = r"""
+    ---
     - host: 
       - {host}
+      gather_facts: False
       tasks:
       - name: Stop old process
-        win_command: powershell.exe Stop-Process -Name ApplicationName
+        win_command: powershell.exe Stop-Process -Name {service}.exe
           
       - name: Run new process
-        win_command: application.exe 
+        win_command: {service}.exe 
         args:
           chdir: D:\iisroot\
     """
@@ -105,34 +103,29 @@ class ParseCommand(object):
     def __init__(self, wechat_msg: str, notify_func, notify_users: list):
         try:
             cmd, host, service = wechat_msg.split(" ")
+            # 这个_cmd是用来判断命令的类型
             self.cmd = cmd
-            self.host = host
             self.service = service
+            self.host = host
         except ValueError:
             self.cmd = wechat_msg
         self.notify_func = notify_func
         self.notify_users = notify_users
 
     def get_cmd_thread(self):
-        if self.cmd == "recycle":
-            print("执行回收IIS命令")
-        elif self.cmd == "restart":
-            print("执行服务相关命令")
-        elif self.cmd == "run":
-            print("执行启动Windows进程服务")
+        if self.cmd == 'recycle':
+            return _RecycleCommand(self.cmd, self.host, self.service, self.notify_func, self.notify_users)
+        elif self.cmd == 'service':
+            pass
+        elif self.cmd == 'run':
+            return _RunWindowsProcess(self.cmd, self.host, self.service, self.notify_func, self.notify_users)
         else:
             print("默认执行系统的命令")
-            return _SystemCommandThread(self.cmd, self.notify_func, self.notify_users)
+            return _SystemCommandThread(self.cmd, "", "", self.notify_func, self.notify_users)
 
 
 if __name__ == '__main__':
-    """"
     async def func(x, y):
         print(x, y)
-    cmd_thread = RecycleCommand("recycle o2o10 shop", func, ["tkggvfhpce2"])
+    cmd_thread = ParseCommand("recycle O2O20 shop", func, ["tkggvfhpce2"]).get_cmd_thread()
     cmd_thread.start()
-    """
-    origin = ParseCommand("service rabbitmq-prod")
-    if origin:
-        print(True)
-
